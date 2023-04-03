c26391b4e7da83ea988829d2c27e2c9d76953b03
João Vitor <contatolimaocode@gmail.com>
2023-04-03 16:44:46 -0300

    feat: added eslint and prettier

--- App.js
+++ App.js
@@ -1,25 +1,23 @@
-import { StatusBar } from "expo-status-bar";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
-}
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#EAE5D1",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#5F1F15",
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#EAE5D1',
+		alignItems: 'center',
+		justifyContent: 'center'
+	},
+	text: {
+		color: '#5F1F15'
+	}
 });
+
+export default function App() {
+	return (
+		<SafeAreaView style={styles.container}>
+			<Text style={styles.text}>Open up App.js to start working on your app!</Text>
+			<StatusBar style="auto" />
+		</SafeAreaView>
+	);
+}
